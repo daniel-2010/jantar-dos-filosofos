1a6f96ea6bba9adb3834e7ad1d3a688cce0293f5
Daniel C <daniel-2.010@hotmail.com>
2020-11-29 15:59:46 -0300

    README.md

--- src/JantarFilosofos/Jantar.java
+++ src/JantarFilosofos/Jantar.java
@@ -1,5 +1,15 @@
 package JantarFilosofos;
 
+/*
+O JANTAR DOS FILÓSOFOS - PROBLEMA DE SINCRONIZAÇÃO EM SISTEMAS OPERACIONAIS
+Um dos problemas clássicos sobre a sincronização na comunicação entre processos e threads em Sistemas Operacionais.
+
+Cinco filósofos estão sentados em uma mesa redonda para jantar. Cada filósofo tem um prato com espaguete à sua frente. 
+Cada prato possui um garfo para pegar o espaguete. O espaguete está muito escorregadio e, para que um filósofo consiga comer, 
+será necessário utilizar dois garfos. 
+
+*/
+
 import java.awt.BorderLayout;
 
 import java.awt.EventQueue;
